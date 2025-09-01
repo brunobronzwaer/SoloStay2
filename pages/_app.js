6547e575f2124112d5fb75938ff2a6cdf7cefe69
brunobronzwaer <beebronzwaer@hotmail.com>
2025-09-01 15:34:46 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Travelpayouts tracking script */}
+        <script
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var script = document.createElement("script");
+                script.async = 1;
+                script.src = 'https://tpembars.com/NDU1MjY5.js?t=455269';
+                document.head.appendChild(script);
+              })();
+            `
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
