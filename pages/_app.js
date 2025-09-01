def015b541166abade7a5f8f8848ad7d47064f0e
brunobronzwaer <beebronzwaer@hotmail.com>
2025-09-01 15:51:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,21 @@
+// pages/_app.js
 import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* Travelpayouts tracking script */}
-        <script
-          data-noptimize="1"
-          data-cfasync="false"
-          data-wpfc-render="false"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                var script = document.createElement("script");
-                script.async = 1;
-                script.src = 'https://tpembars.com/NDU1MjY5.js?t=455269';
-                document.head.appendChild(script);
-              })();
-            `
-          }}
-        />
-      </Head>
+      <Head>{/* plek voor meta-tags etc. */}</Head>
+
+      {/* Laad Travelpayouts JS-bestand direct */}
+      <Script
+        src="https://tpembars.com/NDU1MjY5.js?t=455269"
+        strategy="afterInteractive"
+        data-noptimize="1"
+        data-cfasync="false"
+        data-wpfc-render="false"
+      />
+
       <Component {...pageProps} />
     </>
   );
