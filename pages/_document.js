da84572d1cbfdfaac313786ceb6bf520a8d2f785
brunobronzwaer <beebronzwaer@hotmail.com>
2025-09-01 15:59:48 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,17 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="nl">
       <Head>
-        {/* Travelpayouts tracking code */}
+        {/* Travelpayouts tracking script */}
         <script
+          src="https://tpembars.com/NDU1MjY5.js?t=455269"
           data-noptimize="1"
           data-cfasync="false"
           data-wpfc-render="false"
-          dangerouslySetInnerHTML={{
-            __html: `(function () {
-              var script = document.createElement("script");
-              script.async = 1;
-              script.src = 'https://tpembars.com/NDU1MjY5.js?t=455269';
-              document.head.appendChild(script);
-            })();`,
-          }}
-        />
+          async
+        ></script>
       </Head>
       <body>
         <Main />
